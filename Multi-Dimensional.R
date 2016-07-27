faa8822e4b7a3f43ed9e2c7b8ebb7016b48ffa66
praveen-mg <praveen_mg17@yahoo.co.in>
2016-07-27 16:27:03 +0800

    multi dimensional data

---
+++ Multi-Dimensional.R
@@ -0,0 +1,63 @@
+names(pf)
+library(ggplot2)
+ggplot(aes(x=gender,y=age),data = subset(pf,!is.na(gender)))+
+  geom_boxplot()+
+  stat_summary(fun.y = mean,geom = 'point',shape = 4)
+
+ggplot(aes(x=age,y = friend_count),data = subset(pf,!is.na(gender)))+
+  geom_line(aes(color = gender),stat = 'summary', fun.y = median)
+
+library('dplyr')
+pf_by_age_gender <- group_by(pf,age,gender)
+
+head(pf_by_age_gender)
+
+pf.fc_by_age_gender <- summarise(subset(pf_by_age_gender,!is.na(gender)) ,
+                                 mean_friend_count = mean(friend_count),
+                                 median_friend_count = median(friend_count),
+                                 n = n())
+
+ggplot(aes(x = age, y = median_friend_count),data = subset(pf.fc_by_age_gender,!is.na(gender)))+
+  geom_line(aes(color= gender))
+
+install.packages('reshape2')
+
+library('reshape2')
+
+
+pf.fc_by_age_gender.wide <- dcast(pf.fc_by_age_gender,
+                                  age ~ gender,
+                                  value.var = 'median_friend_count')
+
+head(pf.fc_by_age_gender.wide)
+
+
+ggplot(aes(x=age,y=female/male),data = pf.fc_by_age_gender.wide)+
+  geom_line()+
+  geom_hline(yintercept = 1,alpha = 0.3,color = 'blue')
+
+head(pf)
+
+pf$year_joined <- floor(2014 - (pf$tenure / 365))
+
+?cut
+
+pf$year_joined.bucket <- cut(pf$year_joined,breaks = c(2004,2009,2011,2012,2014))
+table(pf$year_joined.bucket)
+
+
+ggplot(aes(x= age,y= friend_count),data = subset(pf,!is.na(year_joined.bucket)))+
+  geom_line(aes(color = year_joined.bucket),stat = 'summary',fun.y = mean)+
+  geom_line(stat = 'summary',fun.y = mean,linetype = 2)
+
+pf_more_one_day <- subset(pf,tenure >1)
+
+pf_more_one_day$rate <- pf_more_one_day$friend_count/pf_more_one_day$tenure
+
+summary(pf_more_one_day$rate)
+
+with(subset(pf,tenure >= 1),summary(friend_count/tenure))
+
+ggplot(aes(x= age ,y = friendships_initiated/tenure),data = subset(pf,tenure>1))+
+  geom_line(aes(color= year_joined.bucket),stat = 'summary',fun.y = mean)+
+  geom_line(stat = 'summary',fun.y = mean,linetype = 2)
